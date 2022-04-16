2c721e3631093f1dff7f12124b1508cb49133b8c
unknown <nathanazevedo@gec.inatel.br>
2022-04-15 22:42:55 -0300

    project: calculator project

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,33 +1,32 @@
-let display = document.getElementById('display');
+let display = document.getElementById("display");
 
-let buttons = Array.from(document.getElementsByClassName('button'));
+let buttons = Array.from(document.getElementsByClassName("button"));
 
-buttons.map(button =>{
-    button.addEventListener('click',(e)=>{
-        // console.log('clicked');
-        // console.log(e);
-        // console.log(e.target);
-        // console.log(e.target.innerText);
-        switch(e.target.innerText){
-            case 'C':
-                display.innerText = '';
-                break;
-            case '←':
-                //if exists something to be erased it will, otherwise not, in order to avoid errors
-                if(display.innerText){
-                    display.innerText = display.innerText.slice(0,-1);
-                }
-                break;
-            case '=':
-                try{
-                    display.innerText = eval(display.innerText);
-                }catch{
-                    display.innerText = 'Error';
-                }
-                break;
-            default:
-                display.innerText += e.target.innerText;
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    // console.log('clicked');
+    // console.log(e);
+    // console.log(e.target);
+    // console.log(e.target.innerText);
+    switch (e.target.innerText) {
+      case "C":
+        display.innerText = "";
+        break;
+      case "←":
+        //if exists something to be erased it will, otherwise not, in order to avoid errors
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1);
         }
-    });
+        break;
+      case "=":
+        try {
+          display.innerText = eval(display.innerText);
+        } catch {
+          display.innerText = "Error";
+        }
+        break;
+      default:
+        display.innerText += e.target.innerText;
+    }
+  });
 });
-
---
+++ Wheter App/script.js
@@ -0,0 +1,50 @@
+let weather = {
+  apiKey: "0e7aa0cf4983f10fdacd079f8005dab2",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+//searches when pressing button search
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+//let search occurs when pressing enter
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+//start by default on Denver
+weather.fetchWeather("Denver");
